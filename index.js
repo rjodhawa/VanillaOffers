b95bb9809094d2d9cc3c61dc192189cd4f868bd0
raunak-at-857299346259 <rjodhawa@andrew.cmu.edu>
2018-08-19 12:55:40 -0400

    Adding CORS

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
+var cors = require('cors');
+
 // Connect to MongoDB on mlab
 mongoose.connect(configInfo,{ useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
+app.use(cors());
 // Get the route info
 var offerRoutes = require('./app/routes/offerRoutes');
 offerRoutes(app);
